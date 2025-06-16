420ef7674f3e2ab00fde69ebb94e83c99910eb31
aayushhh-operator <chaudhari.aayush1607@gmail.com>
2025-06-16 14:27:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,5 +87,6 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    print("✅ Starting Flask server on http://127.0.0.1:5000")
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
